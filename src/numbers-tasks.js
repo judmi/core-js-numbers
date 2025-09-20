83a19f90cade65c944b1f04928e5433fd0a8c421
Ju Dmitrieva <yulia.dmitrieva@yahoo.com>
2025-09-20 13:47:00 +0300

    fix: floating-point imprecision in getsumofnumbers

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -550,7 +550,7 @@ function getIntegerPartNumber(number) {
  * 0.1, 0.2, 0.3 => 0.6
  */
 function getSumOfNumbers(x1, x2, x3) {
-  return Math.floor(x1 + x2 + x3);
+  return +(x1 + x2 + x3).toFixed(12);
 }
 
 /**
